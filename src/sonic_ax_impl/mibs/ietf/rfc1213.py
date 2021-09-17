ad07720e3b3eb5e8030a14c0a765073c4c9b2751
SuvarnaMeenakshi <50386592+SuvarnaMeenakshi@users.noreply.github.com>
2021-09-16 22:18:16 -0700

    [201911][RFC1213]: Initialize lag oid map in reinit_data (#234)
    updating in update_data. Updating lag oid map in update_data
    can cause descrepancy as interface oid map is updated in
    reinit_data.

--- src/sonic_ax_impl/mibs/ietf/rfc1213.py
+++ src/sonic_ax_impl/mibs/ietf/rfc1213.py
@@ -220,6 +220,10 @@ class InterfacesUpdater(MIBUpdater):
         self.mgmt_oid_name_map, \
         self.mgmt_alias_map = mibs.init_mgmt_interface_tables(self.db_conn[0])
 
+        self.lag_name_if_name_map, \
+        self.if_name_lag_name_map, \
+        self.oid_lag_name_map, _ = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, self.db_conn)
+
     def update_data(self):
         """
         Update redis (caches config)
@@ -230,10 +234,6 @@ class InterfacesUpdater(MIBUpdater):
             if_idx = mibs.get_index_from_str(self.if_id_map[sai_id_key])
             self.if_counters[if_idx] = self.namespace_db_map[namespace].get_all(mibs.COUNTERS_DB, \
                     mibs.counter_table(sai_id), blocking=True)
-
-        self.lag_name_if_name_map, \
-        self.if_name_lag_name_map, \
-        self.oid_lag_name_map, _ = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, self.db_conn)
 
         self.if_range = sorted(list(self.oid_name_map.keys()) +
                                list(self.oid_lag_name_map.keys()) +
