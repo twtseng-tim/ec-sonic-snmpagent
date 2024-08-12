3ec4c6ff00eb6c95106628d073ec76b0e0504e3a
twtseng-tim <59221293+twtseng-tim@users.noreply.github.com>
2024-08-12 15:04:12 +0800

    Support get pvid

--- src/sonic_ax_impl/mibs/ietf/rfc4363.py
+++ src/sonic_ax_impl/mibs/ietf/rfc4363.py
@@ -132,12 +132,94 @@ class FdbUpdater(MIBUpdater):
 
         return self.vlanmac_ifindex_list[right]
 
+class Dot1qPortUpdater(MIBUpdater):
+    def __init__(self):
+        super().__init__()
+        self.db_conn = Namespace.init_namespace_dbs()
+
+        self.if_name_map = {}
+        self.if_alias_map = {}
+        self.if_id_map = {}
+        self.oid_name_map = {}
+        self.sai_lag_map = {}
+        self.ifindex_pvid_map = {}
+        self.ifindex_pvid_list = []
+
+    def reinit_data(self):
+        """
+        Subclass update interface information
+        """
+        (
+            self.if_name_map,
+            self.if_alias_map,
+            self.if_id_map,
+            self.oid_name_map,
+        ) = Namespace.get_sync_d_from_all_namespace(
+            mibs.init_sync_d_interface_tables, self.db_conn
+        )
+
+        self.lag_name_if_name_map, \
+        self.if_name_lag_name_map, \
+        self.oid_lag_name_map,     \
+        _, self.sai_lag_map = Namespace.get_sync_d_from_all_namespace(mibs.init_sync_d_lag_tables, self.db_conn)
+
+
+    def update_data(self):
+        """
+        Update redis (caches config)
+        Pulls the table references for each interface.
+        """
+        self.ifindex_pvid_map = {}
+        self.ifindex_pvid_list = []
+
+        vlan_member_strings = Namespace.dbs_keys(self.db_conn, mibs.CONFIG_DB, "VLAN_MEMBER|*")
+        if not vlan_member_strings:
+            return
+
+        for s in vlan_member_strings:
+            vlan_member = s
+
+            try:
+                _, vlan_id, port_name = s.split("|", maxsplit=2)
+                vlan_id = int(vlan_id.split("Vlan")[1])
+                port_index = (mibs.get_index_from_str(port_name),)
+            except ValueError as e:  # includes simplejson.decoder.JSONDecodeError
+                mibs.logger.error("'CONFIG_DB' includes invalid Vlan member: {}.".format(s))
+                continue
+
+            ent = Namespace.dbs_get_all(self.db_conn, mibs.CONFIG_DB, s, blocking=True)
+            untagged = ent["tagging_mode"] == "untagged"
+            if untagged:
+                self.ifindex_pvid_map[port_index] = vlan_id
+                mibs.logger.info("vid = {}".format(vlan_id))
+            else:
+                if port_index not in self.ifindex_pvid_map.keys():
+                    self.ifindex_pvid_map[port_index] = 0
+
+            self.ifindex_pvid_list.append(port_index)
+        self.ifindex_pvid_list.sort()
+
+    def port_vid(self, sub_id):
+        return self.ifindex_pvid_map.get(sub_id, None)
+
+    def get_next(self, sub_id):
+        right = bisect_right(self.ifindex_pvid_list, sub_id)
+        if right >= len(self.ifindex_pvid_list):
+            return None
+
+        return self.ifindex_pvid_list[right]
+
 class QBridgeMIBObjects(metaclass=MIBMeta, prefix='.1.3.6.1.2.1.17.7.1'):
     """
     'Forwarding Database' https://tools.ietf.org/html/rfc4363
     """
 
     fdb_updater = FdbUpdater()
+    dot1q_port_updater = Dot1qPortUpdater()
 
     dot1qTpFdbPort = \
         SubtreeMIBEntry('2.2.1.2', fdb_updater, ValueType.INTEGER, fdb_updater.fdb_ifindex)
+
+    dot1qPvid = \
+        SubtreeMIBEntry('4.5.1.1', dot1q_port_updater, ValueType.INTEGER, dot1q_port_updater.port_vid)
+
